d11eeb3cb0428c92f4f672836f992f1e00a156e5
Joe <pogzyb@umich.edu>
2019-10-22 19:37:15 -0400

    Rework how cluster is configured

--- setup/03_start_emr_cluster.py
+++ setup/03_start_emr_cluster.py
@@ -2,92 +2,59 @@
 import boto3
 import json
 import os
+from typing import Dict, Union
 
 
-#####################
+###############################################################
 # This script will:
-
+# - download the .zip data files from capitalbikeshare's S3 bucket
+# - unzip each file (into an "archive" or folder of csv's)
+# - put the extracted csv's into the S3 bucket you provide
+#
 # Note:
 # These environment variables are assumed to be available
 # in the shell or terminal where this script is ran:
 #
 # - S3_BUCKET
 # - AWS_REGION
-# -
-# -
-# -
-#####################
+# - AWS_SECRET_KEY_ID
+# - AWS_SECRET_ACCESS_KEY
+###############################################################
 
 
-def get_client(service: str) -> boto3.client:
-    client = boto3.client(service)
-    return client
+def format_s3_bucket(bootstrap_actions: list) -> list:
+    for action in bootstrap_actions:
+        action['ScriptBootstrapAction']['Path'] =\
+            action['ScriptBootstrapAction']['Path'].format(s3_bucket=os.getenv('S3_BUCKET'))
+        action['ScriptBootstrapAction']['Args'][0] = \
+            action['ScriptBootstrapAction']['Args'][0].format(s3_bucket=os.getenv('S3_BUCKET'))
+    return bootstrap_actions
 
 
-def create_spark_cluster(**kwargs) -> None:
-    emr = get_client('emr')
+def create_spark_cluster(configs: Dict[str, Union[str, int]]) -> None:
+
+    emr = boto3.client('emr')
+
     response = emr.run_job_flow(
-        Name=kwargs.get('cluster_name'),
-        ReleaseLabel="emr-5.27.0",
-        Instances={
-            'InstanceGroups': [
-                {
-                    'Name': 'EmrMaster',
-                    'Market': 'SPOT',
-                    'InstanceRole': 'MASTER',
-                    'BidPrice': '0.45',
-                    'InstanceType': kwargs.get('master_node_type'),
-                    'InstanceCount': 1,
-                    'Configurations': [
-                        {
-                            "Classification": "spark-env",
-                            "Configurations": [
-                                {
-                                    "Classification": "export",
-                                    "Properties": {
-                                        "PYSPARK_PYTHON": "/usr/bin/python3.6"
-                                    }
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    'Name': 'EmrCore',
-                    'Market': 'SPOT',
-                    'InstanceRole': 'CORE',
-                    'BidPrice': '0.45',
-                    'InstanceType': kwargs.get('worker_node_type'),
-                    'InstanceCount': kwargs.get('worker_node_count')
-                }
-            ],
-            'Ec2KeyName': kwargs.get('keypair_name'),
-            'KeepJobFlowAliveWhenNoSteps': True
-        },
-        Applications=[{'Name': 'Hadoop'}, {'Name': 'Spark'}],
+        Name=configs.get('name'),
+        ReleaseLabel='emr-5.27.0',
+        Instances=configs.get('instances'),
+        Applications=configs.get('applications'),
         JobFlowRole='EMR_EC2_DefaultRole',
         ServiceRole='EMR_DefaultRole',
         VisibleToAllUsers=True,
-        BootstrapActions=[
-            {
-                'Name': 'setup',
-                'ScriptBootstrapAction': {
-                    'Path': f's3://{os.getenv("S3_BUCKET")}/emr_files/bootstrap_emr.sh',
-                    'Args': [f's3://{os.getenv("S3_BUCKET")}/emr_files', 'emr_files.tar.gz']
-                }
-            }
-        ]
+        BootstrapActions=configs.get('bootstrap-actions')
     )
     if response['ResponseMetadata']['HTTPStatusCode'] == 200:
         print('Success - Creating Cluster!')
     else:
-        print('Uh oh!')
+        print(f'Uh oh!:\n{response}')
     return
 
 
 def main() -> None:
-    config = json.loads('../emr-config.json')
-    create_spark_cluster(**config)
+    configs = json.loads('../emr-config.json')
+    create_spark_cluster(configs=configs)
     return
 
 
