f43990389f7723a48ce554d07fb4db757ad5b601
Joe <pogzyb@umich.edu>
2019-10-21 21:49:52 -0400

    Fix EMR bootstrap script

--- setup/emr_files/bootstrap_emr.sh
+++ setup/emr_files/bootstrap_emr.sh
@@ -1,27 +1,14 @@
 #!/usr/bin/env bash
 
-echo "---> Starting Bootstrap Setup! <---"
+echo "--->  Starting Bootstrap Setup!   <---"
 
-s3files=${1}
+S3_LOCATION=${1}
+EMR_ZIP_FILES=${2}
 
-echo "---> Setting up Python3 Virtual Environment <---"
-sudo yum -y install python36
-mkdir -p /home/hadoop/venv
-cd /home/hadoop/venv
-virtualenv -p /usr/bin/python3.6 py-ml
+echo "--->  Copying Scripts from S3!    <---"
 
+mkdir -p /home/hadoop/bikeshare_ml
+aws s3 cp ${S3_LOCATION}/${EMR_ZIP_FILES} /home/hadoop/${EMR_ZIP_FILES}
+tar zxvf /home/hadoop/${EMR_ZIP_FILES} -C /home/hadoop/bikeshare_ml
 
-echo "---> Copying Scripts from S3 <---"
-aws s3 cp ${s3files}/files.tar.gz /home/hadoop/files.tar.gz
-mkdir -p /home/hadoop/py-ml
-tar zxvf /home/hadoop/files.tar.gz -C /home/hadoop/py-ml
-
-echo "---> Installing Python Libraries <---"
-source /home/hadoop/venv/py-ml/bin/activate
-pip3 install -r /home/hadoop/py-ml/emr-files/requirements.txt
-
-set -a
-. $PYSPARK_PYTHON=/usr/bin/python3.6
-set +a
-
-echo "---> Completed Bootstrap Setup! <---"
+echo "--->  Completed Bootstrap Setup!  <---"
