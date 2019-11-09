519a6f2b76de292c93194e5578e810689dbeb209
Joe <pogzyb@umich.edu>
2019-11-09 16:08:54 -0500

    Add PMML support to notebook and emr

--- setup/emr-files/bootstrap_emr.sh
+++ setup/emr-files/bootstrap_emr.sh
@@ -11,4 +11,14 @@ mkdir -p /home/hadoop/bikeshare
 aws s3 cp ${S3_LOCATION}/${EMR_ZIP_FILES} /home/hadoop/${EMR_ZIP_FILES}
 tar zxvf /home/hadoop/${EMR_ZIP_FILES} -C /home/hadoop/bikeshare
 
+echo "--->  Installing Python Libraries <---"
+
+SPARK_HOME=/usr/local/spark
+JPMML_VERSION=1.5.4
+wget https://github.com/jpmml/jpmml-sparkml/releases/download/${JPMML_VERSION}/jpmml-sparkml-executable-${JPMML_VERSION}.jar \
+    && mv jpmml-sparkml-executable-${JPMML_VERSION}.jar ${SPARK_HOME}/jars/jpmml-sparkml-executable-${JPMML_VERSION}.jar
+
+easy_install-3.6 pip
+pip install py4j==0.10.7 pyspark2pmml==0.5.1
+
 echo "--->  Completed Bootstrap Setup!  <---"
